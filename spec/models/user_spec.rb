44a3f24620e22ff753fd81ceff270dfb18c8a2ed
Diego Garza <dgarza.m93@gmail.com>
2014-10-02 14:12:10 -0500

    Finishes modeling the product model along with user association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,29 @@ describe User do
 
   it { should be_valid }
 
+  it { should have_many(:products) }
+
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token)}
 
+  describe "#products association" do
+
+    before do
+      @user.save
+      3.times { FactoryGirl.create :product, user: @user }
+    end
+
+    it "destroys the associated products on self destruct" do
+      products = @user.products
+      @user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 
   describe "#generate_authentication_token!" do
     it "generates a unique token" do
